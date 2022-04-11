a45598fe20b01143ab968b51df3f616f00a0a3b1
jaimegag <jaimegag@gmail.com>
2022-04-10 21:42:54 -0400

    live-update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + VMware!";
+		return "Greetings from Spring Boot + VMware Tanzu!";
 	}
 
 }
